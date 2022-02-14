4f401c04b36e3e4f3aea44e9f5ece799a4f6fe9e
abbieluper <aluper@byu.edu>
2022-02-14 11:05:40 -0700

    * .gitignore: commit all changes
    * Bookstore.sln:
    * Program.cs:
    * Startup.cs:
    * Book.cs:
    * Bookstore.csproj:
    * Bookstore.sqlite:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * _ViewStart.cshtml:
    * Index.cshtml:
    * About.cshtml:
    * _ViewImports.cshtml:
    * BookstoreContext.cs:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * IBookstoreRepository.cs:
    * EFBookstoreRepository.cs:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.min.css:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+
+//this is the home controller page
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1) // this sets the default of pageNum to 1
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books // pass data to the index file
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x); // DO NOT FORGET TO PASS THE x RIGHT HERE!!!!!!!!!!!!!
+        }
+
+        public IActionResult About()
+        {
+            return View(); 
+        }
+
+    }
+}
---
+++ Bookstore/Models/IBookstoreRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace Bookstore.Models
+{
+    public interface IBookstoreRepository // this must be an INTERFACE instead of a class
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
